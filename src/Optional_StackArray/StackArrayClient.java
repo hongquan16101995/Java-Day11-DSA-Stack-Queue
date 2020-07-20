9f474f2be0ff01051933765cf70b32aecde94893
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-20 10:23:04 +0700

    bigupdateday11

---
+++ src/Optional_StackArray/StackArrayClient.java
@@ -0,0 +1,28 @@
+package Optional_StackArray;
+
+public class StackArrayClient {
+    public static void main(String[] as) throws Exception {
+        StackArray stack = new StackArray(5);
+
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+
+        System.out.println("1. Size of stack after push operations: " + stack.size());
+        System.out.println(stack.isFull());
+//        stack.push(0);
+
+        System.out.printf("2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+
+        System.out.println("\n3. Size of stack after pop operations : " + stack.size());
+//        stack.pop();
+
+        System.out.println(stack.isFull());
+        System.out.println(stack.isEmpty());
+    }
+}
